d4b22d468a9e4a5a41460391f0da6d1b11604c5b
quiquefernandez <43149643+quiquefernandez@users.noreply.github.com>
2018-10-17 18:41:56 -0400

    Rename drivedisk.m to Auxiliary Calculations/drivedisk.m

--- Auxiliary Calculations/drivedisk.m
+++ Auxiliary Calculations/drivedisk.m
@@ -1,14 +1,14 @@
 function d = drivedisk(t,s)
 
 Tm = 100000;            % Torque applied       -       N*m
-Kb = 0;                 % Friction coefficient -       Kg·m^2/(s·rad)
+Kb = 0;                 % Friction coefficient -       KgÂ·m^2/(sÂ·rad)
 m = 1;                  % Mass of the ice      -       Kg
 r = 0.5;                % Position of the ice  -       m
 rad = 3;               % Radius of the disk   -       m
 h = 0.5;                % height of the disk   -       m
 rho = 2700;             % Density of disk      -       Kg/m^3
 mass = rho*pi*rad^2*h;  % mass of the disk     -        Kg
-J = 0.5*mass*rad^2;     % Disk Mass inertia    -       Kg·m^2
+J = 0.5*mass*rad^2;     % Disk Mass inertia    -       KgÂ·m^2
 
 
 d1 = s(2);
