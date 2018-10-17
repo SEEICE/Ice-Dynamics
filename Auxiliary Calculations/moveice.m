6b1081f7b2156ea9ce6141cc35286ae6c4bc9d97
quiquefernandez <43149643+quiquefernandez@users.noreply.github.com>
2018-10-17 18:42:16 -0400

    Rename moveice.m to Auxiliary Calculations/moveice.m

--- Auxiliary Calculations/moveice.m
+++ Auxiliary Calculations/moveice.m
@@ -1,14 +1,14 @@
 function d = moveice(t,s)
 
 u = 500;                    % Torque applied       -       N*m
-Kb = 1000;                  % Friction coefficient -       Kg穖^2/(s穜ad)
-J = 100;                    % Disk Mass inertia    -       Kg穖^2
+Kb = 1000;                  % Friction coefficient -       Kg路m^2/(s路rad)
+J = 100;                    % Disk Mass inertia    -       Kg路m^2
 m = 1;                      % Mass of the ice      -       Kg
 rad = linspace(1, 500, 100; % Radius of the disk   -       m
 h = 0.5;                    % height of the disk   -       m
 rho = 2700;                 % Density of disk      -       Kg/m^3
 mass = rho*pi*rad^2*h;      % mass of the disk     -        Kg
-J = 0.5*mass*rad^2;         % Disk Mass inertia    -       Kg穖^2
+J = 0.5*mass*rad^2;         % Disk Mass inertia    -       Kg路m^2
 
 d1 = s(3);
 d2 = s(4);
